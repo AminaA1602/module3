4aa0bf94cd7e4aa48944942414ff0c7540f6a895
Amina Aweis <AminaAweis1150@gmail.com>
2019-01-30 10:11:08 +0000

    external testing tools

---
+++ ch04_unit_testing/functions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 15:28:08 2019
+
+@author: Skitt
+"""
+
---
+++ ch05_calculator_app/calculator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 30 09:57:12 2019
+
+@author: Skitt
+"""
+
